a094ec829cdade8930a8061c4ea4e267b31b9eb3
Draken-F <139783815+Draken-F@users.noreply.github.com>
2026-06-01 07:44:26 +0100

    Delete repository directory

--- repository/LogisticsLotRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.LogisticsLot;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.UUID;
-
-public interface LogisticsLotRepository extends JpaRepository<LogisticsLot, UUID> {
-}
